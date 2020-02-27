b416fd1803a29deb52e9544fb22f8977b292976b
Rafael Carvalho <rafaeldasaga@gmail.com>
2020-02-26 22:02:30 -0300

    Added getById Person method and 404 exception

--- phone-book-api/src/main/java/com/phonebookapi/controller/PersonController.java
+++ phone-book-api/src/main/java/com/phonebookapi/controller/PersonController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phonebookapi.exception.ResourceNotFoundException;
 import com.phonebookapi.model.Person;
 import com.phonebookapi.repository.PersonRepository;
 
@@ -15,9 +17,15 @@ import com.phonebookapi.repository.PersonRepository;
 public class PersonController {
 	@Autowired
 	PersonRepository personRepository;
-	
+
 	@GetMapping
-	public List<Person> findAllPeople(){
+	public List<Person> findAllPeople() {
 		return personRepository.findAll();
 	}
+
+	@GetMapping("/{id}")
+	public Person getPersonById(@PathVariable Long id) {
+		return personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person", "id", id));
+	}
+
 }
