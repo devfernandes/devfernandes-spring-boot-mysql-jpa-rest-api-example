dd9cdaa2548bd822e823a4154c59222ce28be9fb
Rafael Carvalho <rafaeldasaga@gmail.com>
2020-02-26 22:34:07 -0300

    Added Post method in Person Controller

--- phone-book-api/src/main/java/com/phonebookapi/controller/PersonController.java
+++ phone-book-api/src/main/java/com/phonebookapi/controller/PersonController.java
@@ -1,12 +1,20 @@
 package com.phonebookapi.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.phonebookapi.exception.ResourceNotFoundException;
 import com.phonebookapi.model.Person;
@@ -28,4 +36,10 @@ public class PersonController {
 		return personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person", "id", id));
 	}
 
+	@PostMapping
+	public ResponseEntity<Person> postPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getId()).toUri();
+		return ResponseEntity.created(location).body(savedPerson);
+	}
 }
