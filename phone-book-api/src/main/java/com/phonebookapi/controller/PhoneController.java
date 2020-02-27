51f55c50f42da802341b1784f9df02324ae41cac
Rafael Carvalho <rafaeldasaga@gmail.com>
2020-02-27 19:42:08 -0300

    Added post method to phone controller

--- phone-book-api/src/main/java/com/phonebookapi/controller/PhoneController.java
+++ phone-book-api/src/main/java/com/phonebookapi/controller/PhoneController.java
@@ -1,13 +1,20 @@
 package com.phonebookapi.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.phonebookapi.exception.ResourceNotFoundException;
 import com.phonebookapi.model.Phone;
@@ -28,5 +35,12 @@ public class PhoneController {
 	@GetMapping("/{id}")
 	public Phone getPhoneById(@PathVariable Long id){
 		return phoneRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Phone", "id", id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Phone> postPhone(@Valid @RequestBody Phone phone){
+		Phone createdPhone = phoneRepository.save(phone);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(phone.getId()).toUri();
+		return ResponseEntity.created(uri).body(createdPhone);
 	}
 }
